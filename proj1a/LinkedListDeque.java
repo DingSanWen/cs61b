58988d85948fba765dde8c3aa40bbf43142cbb43
DingSanWen <dingsanwen@126.com>
2017-04-11 09:49:32 +0800

    complete proj1 LinkedListDeque

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -12,6 +12,13 @@ public class LinkedListDeque<type> {
             prev=l;
             next=n;
         }
+        /** this is a recursive helper method*/
+        public type getHelp(int index){
+            if(index==0){
+                return this.item;
+            }
+            return this.next.getHelp(index-1);
+        }
     }
 
     private node sentinel;
@@ -23,7 +30,7 @@ public class LinkedListDeque<type> {
         sentinel.prev=sentinel.next;
         size=1;
     }
-
+    /**Creates an empty linked list deque.*/
     public LinkedListDeque(){
         sentinel=new node(null,null,null);
         sentinel.prev=sentinel;
@@ -54,5 +61,48 @@ public class LinkedListDeque<type> {
 
     public int size(){
         return size;
+    }
+
+    public void printDeque(){
+        node p=sentinel;
+        while(p.next!=sentinel){
+            System.out.print(p.next.item+" ");
+            p=p.next;
+        }
+        System.out.println("");
+    }
+
+    public type removeFirst(){
+        node result=sentinel.next;
+        sentinel.next=sentinel.next.next;
+        sentinel.next.prev=sentinel;
+        result.prev=null;
+        result.next=null;
+        size-=1;
+        return result.item;
+    }
+
+    public type removeLast(){
+        node result=sentinel.prev;
+        sentinel.prev=sentinel.prev.prev;
+        sentinel.prev.next=sentinel;
+        result.prev=null;
+        result.next=null;
+        size-=1;
+        return result.item;
+    }
+
+    public type get(int index){
+
+        node p=sentinel;
+        for(int i = 0;i<=index;i++){
+            p=p.next;
+        }
+        return p.item;
+    }
+
+    public type getRecursive(int index){
+
+        return sentinel.next.getHelp(index);
     }
 }
